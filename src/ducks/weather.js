9842b190c516f556895f5383ac6ac10bfd4cb781
Alan-Miller <alan.ashman@gmail.com>
2017-06-21 00:44:50 -0600

    changes

--- src/ducks/weather.js
+++ src/ducks/weather.js
@@ -1,4 +1,5 @@
 import { buildURL, formatWeatherData } from '../utils/weatherUtils';
+import axios from 'axios';
 
 const initialState = {
   error: false,
@@ -8,14 +9,51 @@ const initialState = {
 };
 
 const RESET = "RESET";
+const SET_WEATHER = "SET_WEATHER";
+const SET_WEATHER_PENDING = "SET_WEATHER_PENDING";
+const SET_WEATHER_FULFILLED = "SET_WEATHER_FULFILLED";
+const SET_WEATHER_REJECTED = "SET_WEATHER_REJECTED";
 
 export default function weather( state = initialState, action ) {
   switch ( action.type ) {
     case RESET: return initialState;
+    case SET_WEATHER_PENDING:
+      return {
+        error: false,
+        loading: true,
+        search: false,
+        weather: {}
+      }
+    case SET_WEATHER_FULFILLED:
+      console.log( 'action.payload', action.payload )
+      return {
+        error: false,
+        loading: false,
+        search: false,
+        weather: action.payload
+      }
+    case SET_WEATHER_REJECTED:
+      return {
+        error: true,
+        loading: false,
+        search: false,
+        weather: {}
+      }
     default: return state;
   }
 }
 
 export function reset() {
   return { type: RESET };
+}
+
+export function setWeather(location) {
+  var url = buildURL(location);
+  const promise = axios.get(url).then(res => {
+    formatWeatherData(res.data);
+  });
+  return {
+    type: SET_WEATHER,
+    payload: promise
+  };
 }
